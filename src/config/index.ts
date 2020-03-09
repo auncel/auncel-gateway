b5a85e0d9acb27641558494f7fd36bf5e6297df5
yidafu <tiaozuwushi@qq.com>
2020-03-09 13:07:41 +0800

    refactor: change service address and app listen port

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,5 +10,6 @@
  * Copyright 2019 - 2020 Mozilla Public License 2.0                          *
  *-------------------------------------------------------------------------- */
 const isLocal = process.env.NODE_ENV !== 'production';
-export const JUDGE_CENTER_TCP_HOST: string = isLocal ? '127.0.0.1' : '101.37.29.47';
-export const JUDGE_CENTER_TCP_PORT: number = isLocal ? 6301 : 6301;
+export const JUDGE_CENTER_TCP_HOST: string = isLocal ? '127.0.0.1' : '49.234.72.82';
+export const JUDGE_CENTER_TCP_PORT: number = isLocal ? 6302 : 6302;
+export const APP_PORT = 6301;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { APP_PORT } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // const config = app.get(ConfigService)
-  await app.listen(3000);
+  await app.listen(APP_PORT);
 }
 bootstrap();
